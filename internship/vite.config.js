690dca3dda3a0ec3c8b8d7418481bb140b1d18ff
Ziya Rashid <144579482+ziyarashidd@users.noreply.github.com>
2025-10-03 18:35:54 +0530

    Update vite.config.js

--- internship/vite.config.js
+++ internship/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 3000
+  }
 })
